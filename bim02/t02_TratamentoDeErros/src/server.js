6871879e1601a9eb3999d4f7fd02f9253d9015c6
Allan Guilherme <153043227+All4mn@users.noreply.github.com>
2026-05-25 11:58:47 -0300

    refatoração
    continuar r08, talvez voltar r07 (server)

---
+++ bim02/t02_TratamentoDeErros/src/server.js
@@ -0,0 +1,31 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+
+import tarefaRoutes from './features/tarefas/tarefa.routes.js'
+import TarefaRepository from './features/tarefas/tarefa.repository.js'
+import TarefaService from './features/tarefas/tarefa.service.js'
+import TarefaController from './features/tarefas/tarefa.controller.js'
+
+const server = Fastify()
+
+server.register(cors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS']
+})
+
+// Composition Root: criação e conexão das dependências
+const repository = new TarefaRepository()
+const service = new TarefaService(repository)
+const controller = new TarefaController(service)
+
+// Registra as rotas, passando o controller via options
+server.register(tarefaRoutes, { controller })
+
+const PORT = 3000
+server.listen({ port: PORT }, (err) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
